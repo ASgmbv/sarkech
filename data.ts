d9e73b91748a94536438c9ce17f3928df496bda3
ASgmbv <alimbeksagymbaev@gmail.com>
2022-12-20 04:01:08 +0600

    .

--- data.ts
+++ data.ts
@@ -3,21 +3,21 @@ import { ITemplate } from "types";
 export const templates: ITemplate[] = [
 	{
 		name: "Hero 1",
-		id: "Section-UdyQp",
+		id: "Section-Yox9y",
 		components: {
-			"Section-UdyQp": {
-				id: "Section-UdyQp",
+			"Section-Yox9y": {
+				id: "Section-Yox9y",
 				type: "Section",
 				parentId: "root",
-				childrenIds: ["H1-wBF9Y", "Paragraph-v86Rm", "Box-OSD7Z"],
+				childrenIds: ["Box-AbwGD"],
 				props: {
-					className: "py-10 px-0 bg-gray-50 flex flex-col items-center",
+					className: "py-10 px-0 bg-gray-50",
 				},
 			},
-			"H1-wBF9Y": {
-				id: "H1-wBF9Y",
+			"H1-l2q36": {
+				id: "H1-l2q36",
 				type: "H1",
-				parentId: "Section-UdyQp",
+				parentId: "Box-AbwGD",
 				childrenIds: [],
 				props: {
 					className:
@@ -25,10 +25,10 @@ export const templates: ITemplate[] = [
 					children: "Talk to your customer, directly on your site",
 				},
 			},
-			"Paragraph-v86Rm": {
-				id: "Paragraph-v86Rm",
+			"Paragraph-DaARK": {
+				id: "Paragraph-DaARK",
 				type: "Paragraph",
-				parentId: "Section-UdyQp",
+				parentId: "Box-AbwGD",
 				childrenIds: [],
 				props: {
 					children:
@@ -37,34 +37,58 @@ export const templates: ITemplate[] = [
 						"text-center max-w-xs text-slate-500 mb-4 text-sm sm:text-base",
 				},
 			},
-			"Button-0uPmu": {
-				id: "Button-0uPmu",
+			"Button-oWBqk": {
+				id: "Button-oWBqk",
 				type: "Button",
-				parentId: "Box-OSD7Z",
+				parentId: "Box-ZuVLn",
 				childrenIds: [],
 				props: {
 					children: "Book demo",
 					className: "font-bold py-2 px-4 text-sm text-emerald-700",
 				},
 			},
-			"Box-OSD7Z": {
-				id: "Box-OSD7Z",
+			"Box-ZuVLn": {
+				id: "Box-ZuVLn",
 				type: "Box",
-				parentId: "Section-UdyQp",
-				childrenIds: ["Button-KKC7P", "Button-0uPmu"],
+				parentId: "Box-AbwGD",
+				childrenIds: ["Button-BBnuX", "Button-oWBqk"],
 				props: {
-					className: "p-2",
+					className: "p-2 mb-1",
 				},
 			},
-			"Button-KKC7P": {
-				id: "Button-KKC7P",
+			"Button-BBnuX": {
+				id: "Button-BBnuX",
 				type: "Button",
-				parentId: "Box-OSD7Z",
+				parentId: "Box-ZuVLn",
 				childrenIds: [],
 				props: {
 					children: "Try here",
 					className:
 						"text-white py-2 px-4 rounded-sm bg-emerald-700 text-sm font-medium",
+				},
+			},
+			"Paragraph-IYBj1": {
+				id: "Paragraph-IYBj1",
+				type: "Paragraph",
+				parentId: "Box-AbwGD",
+				childrenIds: [],
+				props: {
+					children: "Cancel any time within first week",
+					className: "text-xs text-slate-400",
+				},
+			},
+			"Box-AbwGD": {
+				id: "Box-AbwGD",
+				type: "Box",
+				parentId: "Section-Yox9y",
+				childrenIds: [
+					"H1-l2q36",
+					"Paragraph-DaARK",
+					"Box-ZuVLn",
+					"Paragraph-IYBj1",
+				],
+				props: {
+					className: "flex flex-col items-center max-w-5xl mx-auto px-4",
 				},
 			},
 		},
